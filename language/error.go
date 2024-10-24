f64b794dece52d82d38cdb32382cd3bb826017d8
huojian-jan <3300837618@qq.com>
2024-10-24 13:31:20 +0800

    type convertion

--- language/error.go
+++ language/error.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
 
 func isEvent(num int) (bool, error) {
@@ -13,13 +12,15 @@ func isEvent(num int) (bool, error) {
 }
 
 func main() {
-	data := []int{1, 2}
-	for _, v := range data {
-		res, err := isEvent(v)
-		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Println(res)
-		}
-	}
+	// data := []int{1, 2}
+	// for _, v := range data {
+	// 	res, err := isEvent(v)
+	// 	if err != nil {
+	// 		fmt.Println(err.Error())
+	// 	} else {
+	// 		fmt.Println(res)
+	// 	}
+	// }
+
+	convertion_test2()
 }
