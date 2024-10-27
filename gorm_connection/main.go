b240c82d0c9cefa936175fe0c2934615c8aecccd
huojian-jan <3300837618@qq.com>
2024-10-27 12:54:07 +0800

    try gorm

---
+++ gorm_connection/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type user struct {
+	gorm.Model
+	Name string
+	Age  int
+}
+
+func main() {
+	dsn := "root:123456@tcp(118.31.32.197:3306)/gin_test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	db.Create(&user{Name: "zhangsan", Age: 18})
+
+	log.Println("Connected to the database successfully!")
+}
